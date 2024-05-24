c1bf9284d1e2ad2c0605d3f6ce1868331c463301
chelzakaria <chelzakaria20@gmail.com>
2024-05-24 23:34:11 +0100

    rewrite reddit pull data API script
    add new processes in Data processing

--- scripts/get_data_api_reddit.py
+++ scripts/get_data_api_reddit.py
@@ -1,8 +1,10 @@
-import requests
-from datetime import datetime, date
-import pandas as pd
+from datetime import datetime, date, timedelta
+from typing import Optional
 from tqdm import tqdm
 import logging as log
+import pandas as pd
+import requests
+import calendar
 
 log.basicConfig(
     filename="reddit_api_logs.log",
@@ -12,125 +14,115 @@ log.basicConfig(
 )
 
 
-def get_comments_api_reddit(subreddit: str, submission_id: str):
-    fields = [
-        "id",
-        "author",
-        "author_flair_text",
-        "body",
-        "created_utc",
-        "score",
-        "score_hidden",
-        "permalink",
-        "parent_id",
-        "link_id",
-        "subreddit",
-        "subreddit_id",
-    ]
-
-    base_url = f"https://api.pullpush.io/reddit/search/comment/?link_id={submission_id}"
-
-    df = pd.DataFrame(columns=fields)
-    row = []
-    try:
-        data = requests.get(base_url, timeout=60).json()["data"]
-    except:
-        log.error(f"Error in getting comments for {submission_id}")
-        return df
-    for item in data:
-        for field in fields:
-            if field == "created_utc":
-                row.append(datetime.utcfromtimestamp(int(item[field])))
-            else:
-                row.append(str(item[field]))
-        df.loc[len(df)] = row
-        row = []
-
-    return df
-
-
 def get_data_api_reddit(
     subreddit: str,
-    from_date: str,
-    to_date: str,
-    epoch: str,
-    submissions_output_path: str = "subs_output.csv",
-    comments_output_path: str = "comments_output.csv",
+    start_date: str,
+    end_date: Optional[str] = datetime.now().strftime("%Y-%m-%d"),
+    output_path: str = "output.csv",
+    option: str = "submissions",
 ):
-    fields = [
-        "id",
-        "author",
-        "author_flair_text",
-        "title",
-        "selftext",
-        "link_flair_text",
-        "thumbnail",
-        "created_utc",
-        "permalink",
-        "score",
-        "hide_score",
-        "num_comments",
-        "media",
-        "media_embed",
-        "over_18",
-    ]
-    base_url = f"https://api.pullpush.io/reddit/search/submission/?subreddit={subreddit}&sort=asc"
+    """
+    Get data from Reddit API
 
-    if not to_date:
-        to_date = datetime.now().strftime("%Y-%m-%d")
+    Args:
+        subreddit: str: subreddit name
+        start_date: str: start date in format "YYYY-MM-DD"
+        end_date: str: end date in format "YYYY-MM-DD" (default: today)
+        output_path: str: output file path (default: output.csv)
+        option: str: data type to get (submissions or comments) (default: submissions)
 
-    to_date = datetime.strptime(to_date, "%Y-%m-%d")
-    from_date = datetime.strptime(from_date, "%Y-%m-%d")
-    start_day = (datetime.now() - from_date).days
-    end_day = (datetime.now() - to_date).days
-    # start_hour = start_day * 24 + datetime.now().hour
+    Returns:
+        None
+    """
 
-    # diff = to_date - from_date
+    if option == "submissions":
+        base_url = (
+            f"https://api.pullpush.io/reddit/submission/search?subreddit={subreddit}"
+        )
+        fields = [
+            "id",
+            "author",
+            "author_flair_text",
+            "title",
+            "selftext",
+            "link_flair_text",
+            "thumbnail",
+            "created_utc",
+            "permalink",
+            "score",
+            "hide_score",
+            "num_comments",
+            "media",
+            "media_embed",
+            "over_18",
+        ]
+    elif option == "comments":
+        fields = [
+            "id",
+            "author",
+            "author_flair_text",
+            "body",
+            "created_utc",
+            "score",
+            "score_hidden",
+            "permalink",
+            "parent_id",
+            "link_id",
+            "subreddit",
+            "subreddit_id",
+        ]
+        base_url = (
+            f"https://api.pullpush.io/reddit/comment/search?subreddit={subreddit}"
+        )
 
-    # days, seconds = diff.days, diff.seconds
-    # hours = days * 24 + seconds // 3600
+    end_date = datetime.strptime(end_date, "%Y-%m-%d").date()
+    start_date = datetime.strptime(start_date, "%Y-%m-%d").date()
 
-    # with open(output_path, "w") as f:
-    #     f.write(",".join(fields) + "\n")
+    dataframe = pd.DataFrame(columns=fields)
 
-    output_dict = {}
-    subs_dataframe = pd.DataFrame(columns=fields)
-    comments_dataframe = pd.DataFrame()
-    if epoch == "days":
-        for i in tqdm(range(start_day, end_day, -1)):
-            log.info(f"Getting data for {i} days ago")
-            url = f"{base_url}&after={i}d&before={(i-1)}d"
-            try:
-                data = requests.get(url, timeout=20).json()["data"]
-            except:
-                log.error(f"Error in getting data for {i} days ago")
-                continue
+    for i in tqdm(range((end_date - start_date).days)):
+        log.info(
+            f"Getting {option} data from {start_date + timedelta(days=i)} to {start_date + timedelta(days=i + 1)}"
+        )
+        url = f"{base_url}&since={calendar.timegm((start_date + timedelta(days=i)).timetuple())}&until={calendar.timegm((start_date + timedelta(days=i + 1)).timetuple())}"
+        try:
+            data = requests.get(url, timeout=20).json()["data"]
+        except:
+            log.error(
+                f"Error in getting {option} data from {start_date - timedelta(days=i)} to {start_date - timedelta(days=i - 1)}"
+            )
+            continue
+        log.info(f"Founds {len(data)} {option} data")
+        row = []
+        for item in data:
+            for field in fields:
+                if field == "created_utc":
+                    row.append(datetime.utcfromtimestamp(int(item[field])))
+                else:
+                    row.append(str(item[field]))
+            dataframe.loc[len(dataframe)] = row
             row = []
-            for item in data:
-                for field in fields:
-                    if field == "created_utc":
-                        row.append(datetime.utcfromtimestamp(int(item[field])))
-                    else:
-                        row.append(str(item[field]))
-                subs_dataframe.loc[len(subs_dataframe)] = row
-                row = []
-                comments_dataframe = pd.concat(
-                    [
-                        comments_dataframe,
-                        get_comments_api_reddit(subreddit, item["id"]),
-                    ],
-                    ignore_index=True,
-                )
 
-    subs_dataframe.to_csv(submissions_output_path, encoding="utf-8")
-    comments_dataframe.to_csv(comments_output_path, encoding="utf-8")
+    dataframe.to_csv(output_path, encoding="utf-8")
 
 
+start_date = "2009-01-01"
+end_date = "2024-05-24"
+
+# get subreddit submissions
 get_data_api_reddit(
     "morocco",
-    "2022-12-31",
-    "2024-01-25",
-    "days",
-    submissions_output_path="morocco/output_2023.csv",
-    comments_output_path="morocco/comments_2023.csv",
+    start_date,
+    end_date,
+    output_path=f"data/morocco/submissions_{start_date}_{end_date}.csv",
+    option="submissions",
+)
+
+# get subreddit comments
+get_data_api_reddit(
+    "morocco",
+    start_date,
+    end_date,
+    output_path=f"data/morocco/comments_{start_date}_{end_date}.csv",
+    option="comments",
 )
